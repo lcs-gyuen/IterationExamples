98660903a6920e6b525e948c25a62bb7c8c918ef
lcs-gyuen <32303735+lcs-gyuen@users.noreply.github.com>
2017-11-08 23:11:05 -0500

    Update Contents.swift
    finished challenge

--- IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -10,25 +10,17 @@ import Cocoa
 import PlaygroundSupport
 
 //: ## Add your code below
-
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
 
+for i in stride(from: 1, through: 300, by: 20){
+    canvas.drawLine(fromX: i, fromY: 0, toX: 300, toY: i)
+    canvas.drawLine(fromX: i, fromY: 300, toX: 0, toY:i)
+    canvas.drawLine(fromX: i, fromY: 0, toX: 0, toY:300-i)
+    canvas.drawLine(fromX: 300, fromY: i, toX: 300-i, toY:300)
+}
+
 // Below this line, try combining a loop and four statements that draw lines to generate the goal
-
-// Line width
-for y in stride(from: 0, through: 300, by: 20) {
-
-canvas.drawLine(fromX: y, fromY: 0, toX: 300, toY: y)
-    
-canvas.drawLine(fromX: y, fromY: 300, toX: 0, toY: y)
-    
-
-    
-
-    
-    }
-
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
